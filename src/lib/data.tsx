4044294d0822f0e6b250d97e2302a70f6c51ebfc
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2025-06-16 10:53:34 +0700

    feat: create DataTable component and related data structures for IoT items

---
+++ src/lib/data.tsx
@@ -0,0 +1,28 @@
+import { BarangIoT } from "@/lib/Columns";
+
+export const sampleData: BarangIoT[] = [
+  {
+    p: 10,
+    l: 5,
+    t: 3,
+    tipe: "KECIL",
+    amt: 25,
+    barcode: "BRC001"
+  },
+  {
+    p: 20,
+    l: 15,
+    t: 8,
+    tipe: "BESAR",
+    amt: 12,
+    barcode: "BRC002"
+  },
+  {
+    p: 8,
+    l: 4,
+    t: 2,
+    tipe: "KECIL",
+    amt: 50,
+    barcode: "BRC003"
+  }
+];
